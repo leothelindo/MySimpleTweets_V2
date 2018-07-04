301236b916473c132103950debd36f9402545d81
Leonardo Lindo <leojlindo@fb.com>
2018-07-03 17:32:00 -0700

    added compose tweet activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,19 +1,35 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity {
 
     private TwitterClient client;
+    TweetAdapter tweetAdapter;
+    ArrayList<Tweet> tweets;
+    RecyclerView rvTweets;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,14 +37,58 @@ public class TimelineActivity extends AppCompatActivity {
 
         // populating the timeline for twitter
         client = TwitterApp.getRestClient(this);
+
+        // find the RecyclerView
+        rvTweets = (RecyclerView) findViewById(R.id.rvTweet);
+        // init the arraylist (data source)
+        tweets = new ArrayList<>();
+        // construct the adapter from this datasource
+        tweetAdapter = new TweetAdapter(tweets);
+
+        // RecyclerView setup
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        // set the adapter
+        rvTweets.setAdapter(tweetAdapter);
         populateTimeline();
+
+
     }
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public void onComposeAction(MenuItem i) {
+        Intent intent = new Intent(this, ComposeActivity.class);
+        startActivityForResult(intent, 20);
+    }
+
 
     private void populateTimeline(){
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                Log.d("TwitterClient", response.toString());
+                // Log.d("TwitterClient", response.toString());
+                // iterate through the JSON Array
+                // deserialize the JSON object
+
+                for (int i = 0; i < response.length(); i++){
+                    try{
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        tweets.add(tweet);
+                        tweetAdapter.notifyItemInserted(tweets.size() - 1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                // convert object to tweet model
+                // add tweet model to data source
+                // notify adapter we've added an item
             }
 
             @Override
