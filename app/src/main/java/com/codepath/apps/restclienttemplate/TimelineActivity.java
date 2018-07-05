b3a05d860b7a2cb5cc40ce12eee218c49d381e88
Leonardo Lindo <leojlindo@fb.com>
2018-07-05 10:35:48 -0700

    added time ish

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -54,8 +54,6 @@ public class TimelineActivity extends AppCompatActivity {
 
     }
 
-
-
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
