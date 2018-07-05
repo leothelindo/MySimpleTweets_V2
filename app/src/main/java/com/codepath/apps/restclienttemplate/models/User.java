3b31420ec8d452175527f37757dc658a2fa12836
Leonardo Lindo <leojlindo@fb.com>
2018-07-05 16:02:23 -0700

    can compose tweets and post to twitter

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,27 +1,41 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import android.os.Parcel;
+import android.util.Log;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class User {
+@org.parceler.Parcel
+public class User{
     // list attributes
+    public User(){}
 
     public String name;
     public long uid;
     public String screenName;
     public String profileImageUrl;
 
+    protected User(Parcel in) {
+        name = in.readString();
+        uid = in.readLong();
+        screenName = in.readString();
+        profileImageUrl = in.readString();
+    }
+
     // deserialize the JSON
     public static User fromJSON(JSONObject json) throws JSONException{
         User user = new User();
 
+        Log.d("user", json.toString());
         // extract and fill the values
         user.name = json.getString("name");
         user.uid = json.getLong("id");
         user.screenName = json.getString("screen_name");
-        user.profileImageUrl = json.getString("profile_image");
+        user.profileImageUrl = json.getString("profile_image_url");
 
         return user;
 
     }
+
 }
